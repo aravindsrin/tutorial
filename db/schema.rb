9b349fa74e3df23a5d76686075af509359093693
aravindsrin <aravind@francium.tech>
2022-04-05 11:45:39 +0530

    Integrated database relationship between topics and blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_04_050406) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_05_054440) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -31,6 +31,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_04_050406) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
